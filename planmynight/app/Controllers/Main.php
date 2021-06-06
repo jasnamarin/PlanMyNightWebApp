f10ade3810007e2f925443bfdfe360346f9e3861
jovanivko <jovanivko@gmail.com>
2021-06-07 01:02:39 +0200

    modifikacija preferencija

--- planmynight/app/Controllers/Main.php
+++ planmynight/app/Controllers/Main.php
@@ -6,7 +6,13 @@ use App\Models\Entities;
 class Main extends BaseController
 {
 	public function index()
-	{
+	{       
+            $user=$this->session->get('user');
+            if($user==NULL){
+                return redirect()->to(site_url("Login"));
+            }
+            else{
 		echo view("mainPage");
+            }
 	}
 }
