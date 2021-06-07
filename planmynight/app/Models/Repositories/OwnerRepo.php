9c9e52e985b70ef3553276fd4cb7b0cb2d08da69
jovanivko <jovanivko@gmail.com>
2021-06-07 14:20:27 +0200

    repositories

---
+++ planmynight/app/Models/Repositories/OwnerRepo.php
@@ -0,0 +1,19 @@
+<?php namespace App\Models\Repositories;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UserRepo
+ *
+ * @author Filip
+ */
+
+use Doctrine\ORM\EntityRepository;
+
+class OwnerRepo extends EntityRepository {
+    
+}
